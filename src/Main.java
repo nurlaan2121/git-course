301893a3dbe3268e9af8f9a5dbc5176d3c7bdc3c
Nurlan <nurlanabijbillaev@gmail.com>
2023-12-30 10:28:50 +0600

    Addeddddddddddd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello fsdfdsworld!");
+        System.out.println("Hello world!");
+        System.out.println("Hello fsd!");
+        System.out.println("Hello fsdfworld!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hefsdfsdllo world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
